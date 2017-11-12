7e035e17c77a9e9ed1627417e6747e4582fcf067
Josh Petras <joshpetras@gmail.com>
2017-11-12 15:18:00 +0100

    Complete project steps 79 to 83.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,14 +1,27 @@
 const clientId = 'b1893b1ebc9e4491ba2ea34565e2d9f1';
-const secret = '20726b166b4541f1b41e464a871e775e';
+const redirect_uri = 'http://localhost:3000/';
 let accessToken;
 
 const Spotify = {
   getAccessToken() {
-    if(accessToken) {
+    if (accessToken) {
       return new Promise(resolve => resolve(accessToken));
     }
-    // Begin working on project step 79 here...
-  },
 
+    const newAccessToken = window.location.href.match(/access_token=([^&]*)/);
+    const newExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+    if(newAccessToken && newExpiresIn) {
+      accessToken = newAccessToken[1];
+      const expiresIn = Number(newExpiresIn[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
+      window.location = accessUrl;
+    }
+
+  }
+}
 
 export default Spotify;
