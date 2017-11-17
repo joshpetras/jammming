91e92a0fa7780c32af5d3d89d353442423ae22c9
Josh Petras <joshpetras@gmail.com>
2017-11-17 18:43:18 +0100

    Correct url typo in pushState.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -15,7 +15,7 @@ const Spotify = {
       accessToken = newAccessToken[1];
       const expiresIn = Number(newExpiresIn[1]);
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
-      window.history.pushState('Access Token', null, '/jamming/');
+      window.history.pushState('Access Token', null, '/jammming/');
       return accessToken;
     } else {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&show_dialog=true&redirect_uri=${redirect_uri}`;
